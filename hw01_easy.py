e9b80c3913beb85249492db6855f3be6f528d8d6
Krockus <sasha-32@yandex.ru>
2018-11-22 23:14:28 +0300

    дз3

--- hw01_easy.py
+++ hw01_easy.py
@@ -1,1 +1,56 @@
-<пустой файл>
+#Задача 1
+
+
+def GetRoundNum(dec, symb_num) :
+
+	strok = str(dec)
+	lastNum = int(str(dec)[-1])
+	NeedRounding = False
+	mnojitel = 1
+
+	print(strok)
+	GotPoint = False
+	strRes = ''
+	for x in strok :
+		if x == '.' :
+			GotPoint = True
+		elif GotPoint :
+			symb_num -= 1
+			mnojitel /= 10
+		strRes += x
+		if symb_num == -1 :
+			lastNum = int(x)
+			NeedRounding = True
+			#отсекаем последнюю цифру
+			strRes = strRes[:-1]
+			mnojitel *= 10
+			break
+
+	res = float(strRes)
+	if NeedRounding and lastNum >= 5 :
+		res = res + mnojitel
+	
+
+	return res
+
+print(GetRoundNum(123.456, 4))
+	
+			
+#Задача 2   +
+	
+def NumberIsLucky(strok) :
+	strok = str(strok)
+	length = len(strok) 
+	subLeng = length // 2
+	substr1 = strok[:subLeng]
+	if length % 2 > 0:    # число символов нечётно
+		subLeng += 1
+	substr2 = strok[subLeng:]
+
+	sum1 = sum(map(lambda x: int(x),substr1))
+	sum2 = sum(map(lambda x: int(x),substr2))		
+
+	res = False
+	if sum1 == sum2 :
+		res = True
+	return res
--- hw01_hard.py
+++ hw01_hard.py
@@ -1,1 +1,42 @@
-<пустой файл>
+import sys	
+
+def f_Add() :
+	print('Добавляем...')
+	# Вставить сюда рабочий код
+	print('Добавили')
+
+def f_Delete() :
+	print('Удаляем...')
+	# Вставить сюда рабочий код
+	print('Удалили')
+
+def f_Print() :
+	print('Печатаем...')
+	# Вставить сюда рабочий код
+	print('Напечатали')
+	
+def f_Count() :
+	print('Считаем...')
+	# Вставить сюда рабочий код
+	print('Посчитали')
+
+def f_Exit() :
+	sys.exit()
+
+func_names = {'Добавляем': f_Add, 'Удаляем': f_Delete, 'Печатаем': f_Print, 'Считаем': f_Count, 'Выход': f_Exit}
+menu_counts = {1: 'Добавляем',     2: 'Удаляем',         3: 'Печатаем',        4: 'Считаем',      5: 'Выход'}
+
+for k in sorted(menu_counts.keys()) :
+	print(k,':', menu_counts[k])
+
+while True :
+
+	cmd_id = input('Введите команду')
+	try :
+		id_num = int(cmd_id)
+		cmd_id = menu_counts[id_num]
+
+	except :
+		pass
+
+	func_names[cmd_id]()
